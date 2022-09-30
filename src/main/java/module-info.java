8fd00b7137d36bb1e16dcf06085664705f3f0d12
ProfYurko <stashkiv.urasuk@gmail.com>
2022-10-01 00:54:53 +0300

    created project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module application.journal {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens application.journal to javafx.fxml;
+    exports application.journal;
+}

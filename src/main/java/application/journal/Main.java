f42bc062edacae3a462b8549463ae07e3c7de39a
ProfYurko <stashkiv.urasuk@gmail.com>
2022-10-01 20:19:02 +0300

    Refactoring files to start work, also added .css file to the project

--- src/main/java/application/journal/Main.java
+++ src/main/java/application/journal/Main.java
@@ -2,17 +2,19 @@ package application.journal;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("journal.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
